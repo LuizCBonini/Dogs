f4d8292951ea5b4619d30e93e2948c31643b1953
Luiz Bonini <lc.bonini13@gmail.com>
2022-12-03 15:49:28 -0300

    Feat: Cadastrar usuario

--- src/Hooks/useForm.jsx
+++ src/Hooks/useForm.jsx
@@ -5,6 +5,11 @@ const types = {
         regex: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
         ,
         message: 'Preencha um e-mail válido',
+    },
+    senha: {
+        regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#()])[0-9a-zA-Z$*&@#()]{8,}$/
+        ,
+        message: `A senha deve conter no mínimo 8 caracteres 1 letra maiúscula 1 número e 1 símbolo`,
     }
 }
 
