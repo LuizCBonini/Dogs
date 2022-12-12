ca9f0461c3f335bd6ebe71739e430187a1dbc967
Luiz Bonini <lc.bonini13@gmail.com>
2022-12-12 20:13:47 -0300

    Feat: Biblioteca de graficos Victory

--- src/Pages/User/UseStats/UserStats.jsx
+++ src/Pages/User/UseStats/UserStats.jsx
@@ -1,13 +1,33 @@
 import React from 'react'
+import { useEffect } from 'react'
+import { GET_STATS } from '../../../api'
 import Head from '../../../Components/Helper/Head'
+import useFetch from '../../../Hooks/useFetch'
+import Loading from '../../../Components/Helper/Loading/Loading'
+import Error from '../../../Components/Helper/Error'
+import UserStatsGraphs from './UserStatsGraphs'
 
 const UserStats = () => {
-  return (
-    <div>
-      <Head title='Estatísticas'/>
-      UserStats
-    </div>
-  )
+  const {data, error, loading, request} = useFetch()
+
+  useEffect(() => {
+    async function getData() {
+      const {url,options} = GET_STATS();
+      request(url, options)
+    }
+    getData()
+  }, [])
+
+  if(loading) return <Loading/>
+  if(error) return <Error error={error}/>
+  if(data) {
+    return (
+      <div>
+        <Head title='Estatísticas'/>
+        <UserStatsGraphs data={data}/>
+      </div>
+    )
+  } else return null
 }
 
 export default UserStats
